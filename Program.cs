5e6a23927587709524b87888defa1ac8da7355c9
Lior Matsliah <liormatsliah1985@gmail.com>
2021-05-16 11:28:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassLibraryPublish_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Publish.Pack<Member>();
+        }
+    }
+
+    public class Member
+    {
+        public int MembershipId { get; set; }
+        public int NationalityId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Publish.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+namespace ClassLibraryPublish_Core
+{
+    public class Publish
+    {
+        public static void Pack<T>()
+        {
+            var settings = Settings.Default;
+            var lib = Assembly.GetAssembly(typeof(T));
+            var nugetApiKey = settings.NugetApiKey; 
+
+            var libName = lib.GetName();
+            var packagePath = libName.CodeBase; //@"D:\Development\SquadModels\bin\Release\netstandard2.0\publish";
+            var source = settings.NugetUrl;
+
+            var cmdCommand = $@"dotnet nuget push {packagePath} --api-key {nugetApiKey} --source {source}";
+            //$@"dotnet nuget push {packagePath}\SquadModels.{libName.Version.ToString(3)}.nupkg --api-key {nugetApiKey} --source {source}";
+            var process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+
+            process.StandardInput.WriteLine(cmdCommand);
+            process.StandardInput.Flush();
+            process.StandardInput.Close();
+            process.WaitForExit();
+            Console.WriteLine(process.StandardOutput.ReadToEnd());
+
+            Console.ReadKey();
+        }
+    }
+}
